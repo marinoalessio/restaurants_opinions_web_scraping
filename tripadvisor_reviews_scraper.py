32a67d98f3e49e22c4be31dac5a011b78a6c5bf3
Alessio Marino <61171571+marinoalessio@users.noreply.github.com>
2023-01-22 19:53:09 +0100

    Add files via upload

--- tripadvisor_reviews_scraper.py
+++ tripadvisor_reviews_scraper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[48]:
+# In[1]:
 
 
 import sys
@@ -15,91 +15,53 @@ import re
 import pandas as pd
 
 
-# In[45]:
+# In[ ]:
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('-headless')
-options.add_argument('-no-sandbox')
-options.add_argument('-disable-dev-shm-usage')
-options.add_argument("enable-automation")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-extensions")
-options.add_argument("--dns-prefetch-disable")
-options.add_argument("--disable-gpu")
-driver = webdriver.Chrome('chromedriver', options = options)
-
-
-# In[46]:
-
-
-# single case 
-url = "https://www.tripadvisor.com/Restaurant_Review-g60763-d12425739-Reviews-Piccola_Cucina_Estiatorio-New_York_City_New_York.html"
-
-
-# In[49]:
-
-
-driver.get(url)
-
-
-# In[50]:
-
-
-container = driver.find_element(By.XPATH, "//div[@id='REVIEWS']")
-
-
-# In[51]:
-
-
-reviews_div = container.find_elements(By.XPATH, "//div[@class='review-container']")
-
-
-# In[54]:
-
-
-df = pd.DataFrame(columns=['title', 'date', 'rating', 'review'])
-
-
-# In[55]:
-
-
-num_page = 16
-for i in range(0, num_page):
+def get_reviews_from_url(url):
     
-    # expand the review 
-    time.sleep(2)
-    element = driver.find_element(By.XPATH, "//span[@class='taLnk ulBlueLinks']") # expands reviews
-    driver.execute_script("arguments[0].click();", element)
-    time.sleep(2)
+    options = webdriver.ChromeOptions()
+    options.add_argument('-headless')
+    options.add_argument('-no-sandbox')
+    options.add_argument('-disable-dev-shm-usage')
+    options.add_argument("enable-automation")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--dns-prefetch-disable")
+    options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome('chromedriver', options = options)
+    driver.get(url)
+    
+    num_page = 15 # max number of pages to scrape
+    df = pd.DataFrame(columns=['title', 'date', 'rating', 'review'])
+    
+    for i in range(0, num_page):
+    
+        time.sleep(2)
+        element = driver.find_element(By.XPATH, "//span[@class='taLnk ulBlueLinks']") # expand the review 
+        driver.execute_script("arguments[0].click();", element)
+        time.sleep(2)
 
-    container = driver.find_elements(By.XPATH, ".//div[@class='review-container']")
+        container = driver.find_elements(By.XPATH, ".//div[@class='review-container']")
 
-    for j in range(len(container)):
+        for j in range(len(container)):
 
-        title = container[j].find_element(By.XPATH, ".//span[@class='noQuotes']").text
-        date = container[j].find_element(By.XPATH, ".//span[contains(@class, 'ratingDate')]").get_attribute("title")
-        rating = container[j].find_element(By.XPATH, ".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split("_")[3]
-        review = container[j].find_element(By.XPATH, ".//p[@class='partial_entry']").text.replace("\n", " ")
+            title = container[j].find_element(By.XPATH, ".//span[@class='noQuotes']").text
+            date = container[j].find_element(By.XPATH, ".//span[contains(@class, 'ratingDate')]").get_attribute("title")
+            rating = container[j].find_element(By.XPATH, ".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split("_")[3]
+            review = container[j].find_element(By.XPATH, ".//p[@class='partial_entry']").text.replace("\n", " ")
         
-        row = {'title':title, 'date':date, 'rating':rating, 'review':review}
-        df = df.append(row, ignore_index=True)
+            row = {'title':title, 'date':date, 'rating':rating, 'review':review}
+            df = df.append(row, ignore_index=True)
 
-    if (driver.find_elements(By.XPATH, ".//a[@class='nav next ui_button primary disabled']")):
-        break # if there no pages left: pages <= num_pages
+        if (driver.find_elements(By.XPATH, ".//a[@class='nav next ui_button primary disabled']")):
+            break # if there are no pages left, constraint: pages <= num_pages
         
-    # change the page
-    next = driver.find_element(By.XPATH, './/a[@class="nav next ui_button primary"]')
-    driver.execute_script("arguments[0].click();", next)
-    time.sleep(2)
-    #driver.execute_script("arguments[0].scrollIntoView();", next)
-    #next.click()
+        # change the page
+        next = driver.find_element(By.XPATH, './/a[@class="nav next ui_button primary"]')
+        driver.execute_script("arguments[0].click();", next)
+        time.sleep(2)
 
-driver.close()
-
-
-# In[56]:
-
-
-df
+    return df
+    driver.close()
 
